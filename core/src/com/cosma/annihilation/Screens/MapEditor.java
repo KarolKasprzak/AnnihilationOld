29f062d13cb77322535e2b28581b5eb8c02a6972
admin <staineer@gmail.com>
2019-02-13 11:49:22 +0100

    v0.18 own map editor!

--- core/src/com/cosma/annihilation/Screens/MapEditor.java
+++ core/src/com/cosma/annihilation/Screens/MapEditor.java
@@ -1,10 +1,6 @@
 package com.cosma.annihilation.Screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,17 +9,14 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.cosma.annihilation.Annihilation;
 import com.cosma.annihilation.Editor.*;
-import com.cosma.annihilation.Utils.GfxAssetDescriptors;
 import com.kotcrab.vis.ui.VisUI;
 import com.kotcrab.vis.ui.widget.Menu;
 import com.kotcrab.vis.ui.widget.MenuBar;
@@ -32,7 +25,6 @@ import com.kotcrab.vis.ui.widget.file.FileChooser;
 
 public class MapEditor implements Screen, InputProcessor {
 
-    private Skin skin;
     private SpriteBatch batch;
     private ShapeRenderer shapeRenderer;
     private Stage stage;
@@ -45,15 +37,10 @@ public class MapEditor implements Screen, InputProcessor {
     boolean runSim = false;
     int mouseX;
     int mouseY;
-    List list;
-    List objectList;
-    int zoom = 5;
-    private Vector3 touchPosition;
-    boolean canPan = false;
-    boolean canMove = false;
-    boolean canDrag = false;
-    TextButton buttonSave;
-    TextButton buttonLoad;
+
+    private boolean canCameraDrag = false;
+    float zoomLevel = 0.3f;
+
     private GameMap gameMap;
     private CreateMapWindow createMapWindow;
 
@@ -71,11 +58,11 @@ public class MapEditor implements Screen, InputProcessor {
         stage = new Stage(viewportUi);
         VisUI.load(VisUI.SkinScale.X1);
 
-        gameMap = new GameMap(10,10,10);
-        gameMap.getLayers().getLayer(gameMap.createMapLayerAndReturnName()).setColor(Color.OLIVE);
-        gameMap.getLayers().getLayer(gameMap.createMapLayerAndReturnName()).setColor(Color.BLUE);
-        gameMap.getLayers().getLayer(gameMap.createMapLayerAndReturnName()).setVisible(false);
-        mapRender = new MapRender(shapeRenderer,gameMap);
+//        gameMap = new GameMap(10,10,10);
+//        gameMap.getLayers().getLayer(gameMap.createMapLayerAndReturnName()).setColor(Color.OLIVE);
+//        gameMap.getLayers().getLayer(gameMap.createMapLayerAndReturnName()).setColor(Color.BLUE);
+//        gameMap.getLayers().getLayer(gameMap.createMapLayerAndReturnName()).setVisible(false);
+//        mapRender = new MapRender(shapeRenderer,gameMap);
 
 
         createMapWindow = new CreateMapWindow(this);
@@ -92,18 +79,14 @@ public class MapEditor implements Screen, InputProcessor {
         root.setFillParent(true);
         root.setDebug(false);
         stage.addActor(root);
-        stage.addActor(new RightPanel(this));
-
 
 
         camera = new OrthographicCamera();
         viewport = new ExtendViewport(16/1.3f, 9/1.3f,camera);
         camera.update();
+        camera.zoom = 5;
         viewport.apply(true);
         world = new World(new Vector2(0,9),false);
-        camera.zoom = zoom;
-        skin = Annihilation.getAssets().get(GfxAssetDescriptors.skin);
-
 
         im = new InputMultiplexer();
         im.addProcessor(stage);
@@ -137,112 +120,26 @@ public class MapEditor implements Screen, InputProcessor {
     public void createNewMap(int x,int y,int scale){
         gameMap = new GameMap(x,y,scale);
         mapRender = new MapRender(shapeRenderer,gameMap);
+        stage.addActor(new RightPanel(this));
+        setCameraOnMapCenter();
 
     }
+
+    public void loadMap(){
+        stage.addActor(new RightPanel(this));
+    }
+
     public GameMap getMap(){
         return gameMap;
     }
 
-
-    void setupGui (){
-
-        Table table = new Table();
-        stage.addActor(table);
-        table.setFillParent(true);
-        table.setDebug(true);
-
-        //Fps
-
-        //Buttons
-        buttonSave = new TextButton("Save", skin, "default");
-        buttonSave.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-
-            }
-        });
-        buttonLoad = new TextButton("Load", skin, "default");
-        buttonLoad.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-
-            }
-        });
-
-
-
-//        final CheckBox checkBox1 = new CheckBox("Drag & Move", skin, "default");
-//        checkBox1.addListener(new ChangeListener() {
-//            @Override
-//            public void changed (ChangeEvent event, Actor actor) {
-//                Gdx.graphics.setContinuousRendering(checkBox1.isChecked());
-//                if(checkBox1.isChecked()) {
-//                    canDrag = true;
-//                }else {
-//                    canDrag = false;
-//                }
-//            }
-//        });
-        //----------------------------------------------------------------------------------
-        list = new List(skin, "default");
-        String[] myList = {"Select","player","wall","ladder"};
-        list.setItems(myList);
-        list.setAlignment(25);
-        ScrollPane scrollPane = new ScrollPane(list, skin, "default");
-        scrollPane.setColor(0,1,1,1);
-        //-------------------------LayerList-------------------------------------
-
-        List layerList = new List(skin, "default");
-        String[] layerList1 = {"Layer1","Layer2","Layer3"};
-        layerList.setItems(layerList1);
-        layerList.setAlignment(20);
-
-        //-----------------------------------------------------
-//        ButtonGroup buttonGroup = new ButtonGroup(checkBox,checkBox1);
-//        buttonGroup.setMaxCheckCount(1);
-//        buttonGroup.setMinCheckCount(0);
-//        buttonGroup.setUncheckLast(true);
-        //-----------------------------------------------------
-        objectList = new List(skin, "default");
-        objectList.setItems(myList);
-
-        Table tableRightPanel = new Table();
-
-        tableRightPanel.add(layerList);
-        Table tableLeftPanel = new Table();
-        tableLeftPanel.add(scrollPane);
-
-        table.add(tableLeftPanel).left().expandY().expandX();
-        table.add(tableRightPanel).right().expandY();
-
-
-//        table.add(buttonSave).height(30).width(80).left();
-//        table.add(buttonLoad).height(30).width(80).left();
-//        table.row();
-//        table.add(objectList).left().expandX();
-//        table.add(tableRightPanel).right().expandY().width(setScaledWidth(0.4f));
-//        table.add();
-//        table.add();
-//        table.add().expandX();
-//        table.row();
-
-
-
-
-
+    public void setCameraOnMapCenter(){
+        camera.position.set(getMap().getHeight()/2,getMap().getWidth()/2,0);
     }
-
-    public static float setScaledWidth(float scale){
-        return (Gdx.app.getGraphics().getWidth()/12.8f)*scale;
-    }
-
 
     @Override
     public void show() {
         Gdx.input.setInputProcessor(im);
-        touchPosition = new Vector3();
     }
 
     @Override
@@ -305,8 +202,14 @@ public class MapEditor implements Screen, InputProcessor {
 
     @Override
     public boolean keyDown(int keycode) {
+        if (keycode == Input.Buttons.MIDDLE){
+            System.out.println("sadsadsad");
+        }
+
         return false;
     }
+
+
 
     @Override
     public boolean keyUp(int keycode) {
@@ -323,11 +226,26 @@ public class MapEditor implements Screen, InputProcessor {
         Vector3 worldCoordinates = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
         Vector3 vec = camera.unproject(worldCoordinates);
         System.out.println(vec.x + " " + vec.y );
+
+        if (button == Input.Buttons.MIDDLE){
+            canCameraDrag = true;
+            System.out.println(canCameraDrag );
+
+        }
+
+
         return false;
     }
 
+
+
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        if (button == Input.Buttons.MIDDLE){
+            canCameraDrag = false;
+            System.out.println(canCameraDrag );
+        }
+
         return false;
     }
 
@@ -343,6 +261,13 @@ public class MapEditor implements Screen, InputProcessor {
 
     @Override
     public boolean scrolled(int amount) {
+
+        if(amount == 1){
+            camera.zoom += zoomLevel;
+        }
+        else if(amount == -1){
+            camera.zoom -= zoomLevel;
+        }
         return false;
     }
 }
